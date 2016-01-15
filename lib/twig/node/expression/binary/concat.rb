8da25f94951591546338df7d2681f1a3464e0071
Pedro Oliveira <pedro@beta.io>
2016-01-15 16:05:54 +0000

    fixes concat

--- lib/twig/node/expression/binary/concat.rb
+++ lib/twig/node/expression/binary/concat.rb
@@ -1,5 +1,15 @@
 module Twig
   class Node::Expression::Binary::Concat < Twig::Node::Expression::Binary
+
+    def compile(compiler)
+      compiler
+        .raw('(')
+        .subcompile(get_node('left'))
+        .raw('.to_s + ')
+        .subcompile(get_node('right'))
+        .raw('.to_s)')
+    end
+
     def operator(compiler)
       compiler.raw('+')
     end
